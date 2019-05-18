53625ba972b93566d5c3114e9cdd2c30e3665dbf
neretin-trike <hawktrike@gmail.com>
2019-05-18 14:43:44 +0700

    update(js): реализация рекурсивного открытия пустых клеток

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,20 @@ window.onload = function () {
     dom.playingField.width = 500;
     dom.playingField.height = 500;
 
+    dom.playingField.addEventListener('click', (event) => {
+        // console.log(event.offsetX, event.offsetY);
+
+        // let rotX = 10; // Math.log(250-event.offsetX)*2;
+        // let rotY = 10; //Math.log(250-event.offsetY)*2;
+        // console.log(rotX, rotY);
+
+        // dom.playingField.style.transform =
+        //     `perspective(500px) rotateX(${rotX}deg) rotateY(${rotY}deg)`;
+
+        // setTimeout(() => {
+        //     dom.playingField.style.transform = "";
+        // }, 250)
+    });
 
     const getRandomArbitrary = (min, max) => {
         return Math.ceil(Math.random() * (max - min) + min);
@@ -16,6 +30,7 @@ window.onload = function () {
     class Cell {
         constructor(x, y, context, even) {
             this.bomb = null;
+            this.number = 0;
 
             this.x = x;
             this.y = y;
@@ -36,31 +51,103 @@ window.onload = function () {
 
             this.context.fillRect(this.globalX, this.globalY, 50, 50);
         }
-        cellOpen() {
+        cellOpen(cellArray) {
             this.context.fillStyle = "PapayaWhip";
             this.context.fillRect(this.globalX, this.globalY, 50, 50);
 
             this.context.strokeStyle = "Burlywood";
             this.context.lineWidth = 3;
-            this.context.strokeRect(this.globalX+1.5, this.globalY+1.5, 50-3, 50-3);
+            this.context.strokeRect(this.globalX + 1.5, this.globalY + 1.5, 50 - 3, 50 - 3);
 
+            if (this.bomb !== null) {
+                this.bomb.render.call(this);
+            } else if (this.number === 0) {
+                this.check(cellArray);
+            } else {
+                this.renderNumber();
+            }
+        }
+        open() {
+            this.context.fillStyle = "PapayaWhip";
+            this.context.fillRect(this.globalX, this.globalY, 50, 50);
+
+            this.context.strokeStyle = "Burlywood";
+            this.context.lineWidth = 3;
+            this.context.strokeRect(this.globalX + 1.5, this.globalY + 1.5, 50 - 3, 50 - 3);
+        }
+        renderNumber() {
+            this.context.font = "400 28px Arial";
+            this.context.fillStyle = "black";
+            this.context.fillText(this.number, this.globalX + 15, this.globalY + 35);
+        }
+        setNumber() {
+            this.number += 1;
+
+            // this.renderNumber();
+        }
+        check(cellArray) {
+            let directions = [
+                [this.x - 1, this.y - 1],
+                [this.x, this.y - 1],
+                [this.x + 1, this.y - 1],
+                [this.x + 1, this.y],
+                [this.x + 1, this.y + 1],
+                [this.x, this.y + 1],
+                [this.x - 1, this.y + 1],
+                [this.x - 1, this.y],
+            ]
+
+            for (let item of directions) {
+                let [x, y] = item;
+
+                try {
+                    if (cellArray[x][y].number === 0) {
+                        cellArray[x][y].number = "";
+                        cellArray[x][y].open();
+                        cellArray[x][y].check(cellArray);
+                    } else {
+                        cellArray[x][y].open();
+                        cellArray[x][y].renderNumber();
+                    }
+                } catch {
+                    continue;
+                }
+            }
+        }
+        cellLookAround(cellArray) {
+            let directions = [
+                [this.x - 1, this.y - 1],
+                [this.x, this.y - 1],
+                [this.x + 1, this.y - 1],
+                [this.x + 1, this.y],
+                [this.x + 1, this.y + 1],
+                [this.x, this.y + 1],
+                [this.x - 1, this.y + 1],
+                [this.x - 1, this.y],
+            ]
+
+            for (let item of directions) {
+                let [x, y] = item;
+
+                try {
+                    if (cellArray[x][y].bomb === null) {
+                        cellArray[x][y].setNumber();
+                    }
+                } catch {
+                    continue;
+                }
+            }
         }
     }
 
     class Bomb {
-        constructor (context) {
+        constructor(context) {
             this.context = context;
-
-            // this.context.beginPath();
-            // this.context.fillStyle = "Crimson";
-            // this.context.arc(25, 25, 10, 0, Math.PI*2, false);
-            // this.context.closePath();
-            // this.context.fill();
         }
         render() {
             this.context.beginPath();
             this.context.fillStyle = "Crimson";
-            this.context.arc(this.globalX+25, this.globalY+25, 10, 0, Math.PI*2, false);
+            this.context.arc(this.globalX + 25, this.globalY + 25, 10, 0, Math.PI * 2, false);
             this.context.closePath();
             this.context.fill();
         }
@@ -69,7 +156,7 @@ window.onload = function () {
     class Grid {
         constructor(width, height) {
             this.cellArray = [];
-            
+
             let even = 0;
             for (let i = 0; i < width; i++) {
                 this.cellArray[i] = [];
@@ -83,18 +170,27 @@ window.onload = function () {
             }
 
             let bombCount = 10;
-            while(bombCount) {
+            while (bombCount) {
 
-                let randomX = getRandomArbitrary(0, 9);  
-                let randomY = getRandomArbitrary(0, 9);  
+                let randomX = getRandomArbitrary(0, 9);
+                let randomY = getRandomArbitrary(0, 9);
 
-                if ( this.cellArray[randomX][randomY].bomb === null) {
+                if (this.cellArray[randomX][randomY].bomb === null) {
+
                     this.cellArray[randomX][randomY].bomb = new Bomb(ctx);
-                    this.cellArray[randomX][randomY].bomb.render.call(this.cellArray[randomX][randomY]);
+                    // this.cellArray[randomX][randomY].bomb.render.call(this.cellArray[randomX][randomY]);
 
+                    console.log(randomX, randomY);
                     bombCount -= 1;
-                } 
-                console.log(randomX, randomY);
+                }
+            }
+
+            for (let i = 0; i < width; i++) {
+                for (let j = 0; j < height; j++) {
+                    if (this.cellArray[i][j].bomb) {
+                        this.cellArray[i][j].cellLookAround(this.cellArray);
+                    }
+                }
             }
 
 
@@ -108,7 +204,7 @@ window.onload = function () {
                 let answX = (ofX - restX) / 50;
                 let answY = (ofY - restY) / 50;
 
-                this.cellArray[answX][answY].cellOpen();
+                this.cellArray[answX][answY].cellOpen(this.cellArray);
             });
         }
     }
